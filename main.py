687bd398ebefaef3a8801020340467a15bf6d38b
Benjamin Su <benjaminmsu@gmail.com>
2026-07-07 11:07:55 +0800

    finished project

---
+++ main.py
@@ -0,0 +1,40 @@
+import yfinance as yf
+import numpy as np
+
+ticker = yf.Ticker("KO")
+historical_data = ticker.history(start = "2023-05-29", end = "2025-05-29")
+
+# First step--final closing price
+S0 = historical_data["Close"].iloc[-1]
+
+# Calculate the daily returns as a decimal
+returns = historical_data["Close"].pct_change().dropna()
+
+# The mean of the returns for the average expected annual growth (drift)
+mu = returns.mean() * 252
+
+# The annualized volatility (standard deviation of daily percentage returns times the sqrt of the number of steps)
+sigma = returns.std() * (252 ** 0.5)
+
+# The size of each time step in years
+dt = 1/252
+
+paths = 30000
+days = 252
+
+# The random number array drawn fresh for each step and path(shock)
+ran_num = np.random.default_rng(seed = 42)
+Z_arr = ran_num.normal(loc = 0.0, scale = 1.0, size = (paths, days))
+
+# Creates the second part of the formula for each entry
+full_arr = np.exp((mu - (sigma ** 2) / 2) * dt + (sigma * np.sqrt(dt) * Z_arr))
+
+# Creates the price paths by multiplying last day's price times next
+full_arr = S0 * np.cumprod(full_arr, axis = 1)
+
+final_pred = full_arr[:, -1].mean()
+print(f"The final prediction for the stock in a year (5/29/2026) is {round(final_pred, 2)}")
+
+final_prices = full_arr[:, -1]
+for p in [5, 25, 50, 75, 95]:
+    print(f"{p}th percentile: {np.percentile(final_prices, p):.2f}")
